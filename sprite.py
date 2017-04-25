e9b118e36345d75c3e1277f37bfb09d7a4604666
David Skoda <dskoda1@binghamton.edu>
2017-04-24 21:19:00 -0400

    first push

---
+++ sprite.py
@@ -0,0 +1,61 @@
+# INTIALISATION
+import pygame, math, sys
+from pygame.locals import *
+screen = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption('Traffic sim')
+clock = pygame.time.Clock()
+class CarSprite(pygame.sprite.Sprite):
+    MAX_FORWARD_SPEED = 10
+    MAX_REVERSE_SPEED = 10
+    ACCELERATION = 2
+    TURN_SPEED = 5
+
+    def __init__(self, image, position):
+        pygame.sprite.Sprite.__init__(self)
+        surface = pygame.transform.scale(pygame.image.load(image), (30, 20))
+        self.src_image = pygame.transform.rotate(surface, 90)
+        self.position = position
+        self.speed = self.direction = 0
+        self.k_left = self.k_right = self.k_down = self.k_up = 0
+
+    def update(self, deltat):
+        self.speed += (self.k_up + self.k_down)
+        if self.speed > self.MAX_FORWARD_SPEED:
+            self.speed = self.MAX_FORWARD_SPEED
+        if self.speed < -self.MAX_REVERSE_SPEED:
+            self.speed = self.MAX_REVERSE_SPEED
+
+        self.direction += self.k_right + self.k_left
+        x, y = self.position
+        rad = self.direction * math.pi / 180
+
+        x += -self.speed * math.sin(rad)
+        y += -self.speed * math.cos(rad)
+
+        self.position = (x, y)
+        self.image = pygame.transform.rotate(self.src_image, self.direction)
+        self.rect = self.image.get_rect()
+
+        self.rect.center = self.position
+
+# CREATE A CAR AND RUN
+rect = screen.get_rect()
+car = CarSprite('img/car-top.png', rect.center)
+car_group = pygame.sprite.RenderPlain(car)
+
+while 1:
+    deltat = clock.tick(30)
+    for event in pygame.event.get():
+        if not hasattr(event, 'key'): continue
+        down = event.type == KEYDOWN
+        if event.key == K_RIGHT: car.k_right = down * -5
+        elif event.key == K_LEFT: car.k_left = down * 5
+        elif event.key == K_UP: car.k_up = down * 2
+        elif event.key == K_DOWN: car.k_down = down * -2
+        elif event.key == K_ESCAPE: sys.exit(0)
+
+
+    screen.fill((0, 0, 0))
+    car_group.update(deltat)
+    car_group.draw(screen)
+    pygame.display.flip()
